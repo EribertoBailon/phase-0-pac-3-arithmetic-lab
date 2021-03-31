c3e557d2bfc1aef7f571ea46952fad8c50a347e5
Eriberto <ebailon0319@gmail.com>
2021-03-31 00:24:45 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(Number1, Number2) {
+    return (Number1 + Number2)
+}
+function subtract(Number1, Number2) {
+    return (Number1 - Number2)
+}
+function multiply(Number1, Number2) {
+    return (Number1 * Number2 )
+}
+function divide(Number1, Number2) {
+    return (Number1 / Number2 )
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat (n, 10);
+}
